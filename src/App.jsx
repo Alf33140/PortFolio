3d8bda427f0b9d1bf592f06e3adcaad49ef39ee8
Alf33140 <alf2kdavy@yahoo.com>
2026-04-22 16:27:25 +0200

    first commit debut de projet PorteFolio

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {Hero} from "./sections/Hero";
+import {Navbar} from "./layout/Navbar";
+import {Experience} from "./sections/Experience";
+import {Projects} from "./sections/Projects";
+import {Contact} from "./sections/Contact";
+import {Testimonials} from "./sections/Testimonials";
+import {About} from "./sections/About";
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="min-h-screen overflow-x-hidden">
+        <Navbar />
+        <main>
+          <Hero />
+          <Experience />
+          <Projects />
+          <Contact />
+          <Testimonials />
+          <About />
+
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+const navLinks = [
+  { href: "#about", label: "About"},
+  { href: "#projects", label: "Projects"},
+  { href: "#experience", label: "Experience"},
+  { href: "#testimonials", label: "Testimonials"},
+]
+export const Navbar = () => {
+
+    return (
+      <header>
+        <nav>
+          <a>
+            Portfolio <span>  </span>
+
+          </a>
+          {/* Desktop Nav */}
+          <div>
+            <div>
+              
+              {navLinks.map((link, index)=>( <a key = {index} href={link.label}>{link.label}</a>
+              ))}
+            
+            </div>
+          </div>
+        </nav>
+      </header>
+    )
+  }
+
+export default Navbar
