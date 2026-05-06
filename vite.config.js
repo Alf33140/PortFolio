42262943c3f10b51b0f2075098546b88cd4f1d2d
Alf33140 <alf2kdavy@yahoo.com>
2026-05-06 11:06:29 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { fileURLToPath, dirname } from 'node:url';
 
-const __dirname = import.meta.dirname;
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
